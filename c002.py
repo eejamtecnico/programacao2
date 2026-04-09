197be736a96c64778b71771089c8de52625fab38
Berg Dantas <bergdantas@msn.com>
2026-04-09 03:59:56 -0300

    atividade

---
+++ c002.py
@@ -0,0 +1,26 @@
+a = 10
+b = 10.5
+c = "Python"
+d = True
+
+print('Valor de a:', a)
+print('Tipo de a:', type(a))
+print('Endereço de a:', id(a))
+print()
+print('Valor de b:', b)
+print('Tipo de b:', type(b))
+print('Endereço de b:', id(b))
+print()
+print('Valor de c:', c)
+print('Tipo de c:', type(c))
+print('Endereço de c:', id(c))
+print()
+print('Valor de d:', d)
+print('Tipo de d:', type(d))
+print('Endereço de d:', id(d))
+
+#Qual variável armazena um número inteiro?
+#Qual variável armazena texto?
+#O que a função type() retorna?
+#O que você percebe sobre os valores de id()?
+#Duas variáveis com o mesmo valor podem ter o mesmo endereço?
