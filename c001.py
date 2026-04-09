dc9e9dd8ce3b7b6a608310cc7999d3b5b2be06d7
Berg Dantas <bergdantas@msn.com>
2026-04-09 03:38:19 -0300

    variáveis: tipos e endereço

---
+++ c001.py
@@ -0,0 +1,17 @@
+# declara variáveis
+x=3
+y=3.142128
+w='Oi'
+z=True
+
+# escreve as classes dos quatro tipos de dados primitivos do python
+print(type(x))
+print(type(y))
+print(type(w))
+print(type(z)) 
+
+# escreve, na tela, os endereços de memória que as variáveis estão alocadas
+print(id(x))
+print(id(y))
+print(id(w))
+print(id(z))
